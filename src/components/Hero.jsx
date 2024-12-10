ce7af7238d1d3a30dc8df91057b9d93b5a2820b7
Ishola Deborah <82146998+Ishola-Deborah@users.noreply.github.com>
2024-12-10 12:19:14 +0100

    ...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,11 +25,13 @@ const Hero = () => {
         >
           Contact With Me
         </button>
-        <button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+        <a className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full" href="/src/assets/DeboResume.pdf">Resume</a>
+        {/* <button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
-        ><a href="c:\Users\USER\Documents\DeboResume.pdf"></a>
+        >
           Resume
-        </button>
+        </button> */}
 
         
       </div>
