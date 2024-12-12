381b60df438454fa882225a455336cfe3c08bfec
Ishola Deborah <82146998+Ishola-Deborah@users.noreply.github.com>
2024-12-12 16:10:00 +0100

    resume-updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = ({ isMenuOpen }) => {
         <a
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
-          href="/src/assets/DeboResume.pdf"
+          href="/DeboResume.pdf"
           target="_blank"
         >
           Resume
