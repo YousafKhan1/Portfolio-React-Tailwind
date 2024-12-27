5b79653b6e062e05965817cb3f590f534ab614d7
nikstidke253 <nikitatidke50@gmail.com>
2024-12-27 17:42:43 +0530

    new update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,10 @@ import HeroImage from '../assets/hero-image.png'
 const Hero = () => {
   return (
     <div className='bg-black text-white text-center py-16'>
-        <img src={HeroImage} alt="" 
-        className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform 
-        transition-transform duration-300 hover:scale-105'/>
+        
         <h1 className='text-4xl font-bold'>
             I'm {" "}
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Mohammad Yousof</span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Nikita Tidke</span>
             , Full-Stack Developer
         </h1>
         <p className='mt-4 text-lg text-gray-300'>
